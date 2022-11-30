e17639933d4cbb7699163a2cbbb8daf599fdadcf
Kashif Riaz <kashifriazgh@gmail.com>
2022-11-29 17:48:53 -0800

    very first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function index() {
+  return (
+    <>
+      <div className="text-blue-600 text-3xl">Made with tailwindCSS</div>
+      <div className="w-96 h-96 bg-red-400 border-4 border-green-400">
+        <h2 className="text-9xl">hello world</h2>
+      </div>
+    </>
+  );
+}
