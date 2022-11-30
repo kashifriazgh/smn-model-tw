2cc8d44132cc4abd0aeb2c445c29a69618f6064f
Kashif Riaz <kashifriazgh@gmail.com>
2022-11-29 19:43:37 -0800

    dark mode testing

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,11 @@ import React from 'react';
 export default function index() {
   return (
     <>
-      <div className="text-blue-600 text-3xl">Made with tailwindCSS</div>
-      <div className="w-96 h-96 bg-red-400 border-4 border-green-400">
-        <h2 className="text-9xl">hello world</h2>
+      <div className="bg-slate-100 dark:bg-slate-800">
+        <div className="text-blue-600 text-3xl">Made with tailwindCSS</div>
+        <div className="w-96 h-96 bg-red-400 border-4 border-green-400">
+          <h2 className="text-9xl">hello world</h2>
+        </div>
       </div>
     </>
   );
